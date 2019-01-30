a55f767268d6dad7235b1ae98c54432a80baa068
Anwesh43 <anweshthecool0@gmail.com>
2019-01-30 22:09:11 +0530

    adding constants and importing libraries

--- bilinesixtyview/src/main/java/com/anwesh/uiprojects/bilinesixtyview/BiLineSixtyView.kt
+++ bilinesixtyview/src/main/java/com/anwesh/uiprojects/bilinesixtyview/BiLineSixtyView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.bilinesixtyview
 /**
  * Created by anweshmishra on 30/01/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#34CF93")
+val backColor : Int = Color.parseColor("#BDBDBD")
