8e4670a8ea4b7b8d670b60e4ae8157e7a2090267
Anwesh43 <anweshthecool0@gmail.com>
2019-01-30 22:39:47 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbilinesixtyview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbilinesixtyview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbilinesixtyview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bilinesixtyview.BiLineSixtyView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiLineSixtyView.create(this)
     }
 }
